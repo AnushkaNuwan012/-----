5d8302489e6c8256daf8de6aa6f277497be61756
AnushkaNuwan012 <115883833+AnushkaNuwan012@users.noreply.github.com>
2022-10-21 14:31:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94714188730'] //ur owner number
-global.ownername = "nipuna rangana" //ur owner name
-global.ytname = "YT: 𝐂𝐈𝐁𝐄𝐑 𝐓𝐄𝐂𝐇" //ur yt chanel name
-global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['+94766588513'] //ur owner number
+global.ownername = "𝔸ℕ𝕌𝕊ℍ𝕂𝔸 ℕ𝕌𝕎𝔸ℕ" //ur owner name
+global.ytname = "YT: Nuwan Priyantha 01" //ur yt chanel name
+global.socialm = "GitHub: AnushkaNuwan012" //ur github or insta name
+global.location = "Sri Lanka , Central Province , Kandy , Wattegama" //ur location
 
 //bot bomdy
-global.owner = ['94714188730'] //ur number 
-global.ownertag = '94714188730' //ur tag number
-global.botname = '📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶' //ur bot name
-global.linkz = "https://youtu.be/3J9-8U8St3U" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/3J9-8U8St3U" //ur website to be displayed
-global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark packname
-global.author = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark author
+global.owner = ['+94766588513'] //ur number 
+global.ownertag = '+94766588513' //ur tag number
+global.botname = '𝔹𝕝𝕦𝕖 𝕃𝕚𝕠𝕟 𝔹𝕠𝕥' //ur bot name
+global.linkz = "https://youtube.com/channel/UCWZ9GQpgWTUhF2NI0BUjHmA" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCWZ9GQpgWTUhF2NI0BUjHmA" //ur website to be displayed
+global.botscript = 'https://github.com/AnushkaNuwan012' //script link
+global.reactmoji = "🤭" //ur menu react emoji
+global.themeemoji = "😇" //ur theme emoji
+global.packname = "𝔹𝕝𝕦𝕖 𝕃𝕚𝕠𝕟 𝔹𝕠𝕥" //ur sticker watermark packname
+global.author = "𝔹𝕝𝕦𝕖 𝕃𝕚𝕠𝕟 𝔹𝕠𝕥" //ur sticker watermark author
 global.menumoji ="💖"
 global.menuimg = "https://i.ibb.co/0mDLtyr/f2337149532299-58b7611d91faf.jpg" //ur alive img link [https://i.ibb.co]
 global.alive = "https://raw.githubusercontent.com/Nipuna-apps/Blue-Lion-Bot-V2/main/LioneMedia/video/Lione-bot.mp4" //ur alive video or image link [github.com]
